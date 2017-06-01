1ed0c9d95ebdc1e5bf1cc6781b2259a70140e1b5
Junior <nog.junior84@gmail.com>
2017-06-01 17:37:13 -0300

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
-  get 'order/new'
-
-  get 'order/create'
-
-  get 'order/index'
-
   resources :users
   resources :products
+
+  root to: 'home#index'
+
+  post 'order/create'
+  get 'order/new'
+  get 'order/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
