3cfbc0cc82bc59e7c5d0ee51447bcae434285d7a
Junior <nog.junior84@gmail.com>
2017-06-01 20:40:23 -0300

    PagSeguro Config

---
+++ config/pagseguro.rb
@@ -0,0 +1,6 @@
+PagSeguro.configure do |config|
+  config.token       = "2DC455CCF0034741B51CAE3FFEE25C86"
+  config.email       = "nogueira_junior@yahoo.com.br"
+  config.environment = :sandbox # ou :sandbox. O padrão é production.
+  config.encoding    = "UTF-8"
+end
