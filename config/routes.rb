6876cb4c4a6bde930717eb30f8b216d28471c7b2
Junior <nog.junior84@gmail.com>
2017-06-01 17:26:39 -0300

    Controller Order

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'order/new'
+
+  get 'order/create'
+
+  get 'order/index'
+
   resources :users
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
